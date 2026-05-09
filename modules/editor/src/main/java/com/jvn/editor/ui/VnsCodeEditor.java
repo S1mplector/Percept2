a6068a30d72730aec6a5cbe306e90d7bb4800fac
S1mplector <mehmetogluilgaz07@gmail.com>
2026-05-09 15:58:43 +0200

    Suggest VNS divide command

--- modules/editor/src/main/java/com/jvn/editor/ui/VnsCodeEditor.java
+++ modules/editor/src/main/java/com/jvn/editor/ui/VnsCodeEditor.java
@@ -1283,6 +1283,7 @@ public class VnsCodeEditor extends BorderPane {
       out.add(new CodeAutoCompleter.Suggestion("[inc "));
       out.add(new CodeAutoCompleter.Suggestion("[dec "));
       out.add(new CodeAutoCompleter.Suggestion("[mul "));
+      out.add(new CodeAutoCompleter.Suggestion("[div "));
       out.add(new CodeAutoCompleter.Suggestion("[persistent "));
       out.add(new CodeAutoCompleter.Suggestion("[if "));
       out.add(new CodeAutoCompleter.Suggestion("[elif "));
