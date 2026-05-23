052026d18184b0f49d1393c29646cebdc50eea59
perumames <perumames@gmail.com>
2026-05-23 15:31:42 +0200

    Add simple specification

--- modules/core/src/main/java/com/jvn/core/diagnostics/runtime_logs/warnings/WarningCommand.java
+++ modules/core/src/main/java/com/jvn/core/diagnostics/runtime_logs/warnings/WarningCommand.java
@@ -1,5 +1,10 @@
 package com.jvn.core.diagnostics.runtime_logs.warnings;
 
+/**
+ * Follows the Command design pattern.
+ * Contains the information needed to log a warning,
+ * as well as the method that actually executes the warning logging process
+ */
 public interface WarningCommand {
 
 
