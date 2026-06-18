15566239665f66115aa030151bcfdde3fd16694b
perumames <perumames@gmail.com>
2026-06-18 23:40:18 +0200

    Add test for runtime strategy log creation

---
+++ modules/core/src/test/java/com/jvn/core/diagnostics/RuntimeLogsTest.java
@@ -0,0 +1,30 @@
+package com.jvn.core.diagnostics;
+
+import com.jvn.core.diagnostics.runtime_logs.logging_strategies.LogCLI;
+import com.jvn.core.diagnostics.runtime_logs.warnings.WarningSubscriber;
+import com.jvn.core.diagnostics.runtime_logs.warnings.warning_facades.WarningFacade;
+import com.jvn.core.diagnostics.runtime_logs.warnings.warning_facades.WarningManager;
+import com.jvn.core.diagnostics.runtime_logs.warnings.warning_factories.UnknownExpressionWarningFactory;
+import com.jvn.core.diagnostics.runtime_logs.warnings.warning_strategies.WarningLoggingStrategy;
+import org.junit.jupiter.api.Test;
+
+public class RuntimeLogsTest {
+
+    /**
+     * Super general test, ignore, not a real test
+     */
+    @Test
+    public void testRuntimeLogs() {
+        LogCLI cli = new LogCLI();
+
+        WarningLoggingStrategy strategy = new WarningLoggingStrategy();
+        strategy.addLoggingStrategy(cli);
+
+        WarningFacade warningManager = new WarningManager();
+        warningManager.setStrategy(strategy);
+
+        WarningSubscriber subscriber = new WarningSubscriber(warningManager);
+
+        subscriber.onWarningEvent(new UnknownExpressionWarningFactory("INVALID", "Unit Test"));
+    }
+}
